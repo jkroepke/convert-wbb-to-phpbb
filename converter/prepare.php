b5ce1e9f92f31a2f46e59c190b4849a956f1b75e
Jan <jan@2moons.cc>
2014-01-22 23:52:46 +0100

    replace silly fetch_all with traditional while/fetch_assoc way.

--- converter/prepare.php
+++ converter/prepare.php
@@ -8,20 +8,30 @@
 
 $rootUser        = $phpBBDb->query("SELECT * FROM {$phpBBMySQLConnection['prefix']}users WHERE user_type = 3;")->fetch_assoc();
 // save all users
-$defaultUsers    = $phpBBDb->query("SELECT * FROM {$phpBBMySQLConnection['prefix']}users WHERE user_type = 2;")->fetch_all();
+$phpBBDefaultUsers          = array();
+$phpBBDefaultUsersDbResult  = $phpBBDb->query("SELECT * FROM {$phpBBMySQLConnection['prefix']}users WHERE user_type = 2;");
+while($user = $phpBBDefaultUsersDbResult->fetch_assoc())
+{
+    $phpBBDefaultUsers[]    = $user;
+}
+$phpBBDefaultUsersDbResult->close();
 
-echo '.';
+output('row');
 
 // delete the admin and demo posts.
 $phpBBDb->query("TRUNCATE {$phpBBMySQLConnection['prefix']}acl_users;");
+output('row');
 $phpBBDb->query("TRUNCATE {$phpBBMySQLConnection['prefix']}topics_posted;");
+output('row');
 $phpBBDb->query("TRUNCATE {$phpBBMySQLConnection['prefix']}topics;");
+output('row');
 $phpBBDb->query("TRUNCATE {$phpBBMySQLConnection['prefix']}forums;");
+output('row');
 $phpBBDb->query("TRUNCATE {$phpBBMySQLConnection['prefix']}posts;");
+output('row');
 $phpBBDb->query("TRUNCATE {$phpBBMySQLConnection['prefix']}users;");
-
-echo '.';
+output('row');
 
 $phpBBDb->query("DELETE FROM {$phpBBMySQLConnection['prefix']}acl_groups WHERE forum_id != 0;");
-
-echo '.';
+output('row');
+output('end');
