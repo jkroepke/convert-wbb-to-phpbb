c8075f58bd5603011e800672ba0dcf93be10ed80
fc92 <fabio.chimienti@googlemail.com>
2014-01-23 21:23:17 +0100

    Fixed avatar converter

--- converter/user-avatar.php
+++ converter/user-avatar.php
@@ -7,12 +7,12 @@ while($wbbAvatar = $wbbAvatars->fetch_assoc())
     $phpBBAvatar = array(
         'user_avatar_height'    => $wbbAvatar['height'],
         'user_avatar_width'     => $wbbAvatar['width'],
-        'user_avatar'           => $phpBBDb->real_escape_string($phpBBConfig['avatar_salt']."_".$wbbAvatar['userID'].".".$wbbAvatar['avatarExtension']),
+        'user_avatar'           => $phpBBDb->real_escape_string($wbbAvatar['userID']."_".time().".".$wbbAvatar['avatarExtension']),
         'user_avatar_type'      => 1
     );
 
     $wbbAvatarPath = $wbbPath.'wcf/images/avatars/avatar-'.$wbbAvatar['avatarID'].'.'.$wbbAvatar['avatarExtension'];
-    $phpBBAvatarPath = $phpBBPath.$phpBBConfig['avatar_path'].'/'.$phpBBAvatar['user_avatar'];
+    $phpBBAvatarPath = $phpBBPath.$phpBBConfig['avatar_path'].'/'.$phpBBConfig['avatar_salt']."_".$wbbAvatar['userID'].".".$wbbAvatar['avatarExtension'];
 
     //TODO: phpBB Pfade vielleicht leeren.
 
