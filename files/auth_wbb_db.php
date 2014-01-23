370019b70e64883869307543a82e1e5fbb115e36
Jan <jan@2moons.cc>
2014-01-23 01:01:33 +0100

    fixed auth plugin

--- files/auth_wbb_db.php
+++ files/auth_wbb_db.php
@@ -10,12 +10,11 @@ if (!defined('IN_PHPBB'))
 
 /*
 The auth plugin just works, if the wbb has default ENCRYPTION options.
-
+*/
 define('ENCRYPTION_ENABLE_SALTING', 1);
 define('ENCRYPTION_ENCRYPT_BEFORE_SALTING', 1);
 define('ENCRYPTION_METHOD', 'sha1');
 define('ENCRYPTION_SALT_POSITION', 'before');
-*/
 
 require 'auth_db.php';
 
@@ -27,15 +26,15 @@ function login_wbb_db($username, $password, $ip = '', $browser = '', $forwarded_
         INNER JOIN " . USERS_WBB_PASSWORDS_TABLE . " wbbpw USING (user_id)
 		WHERE username_clean = '" . $db->sql_escape($username_clean) . "'";
 
-
     $result = $db->sql_query($sql);
     $row = $db->sql_fetchrow($result);
     $db->sql_freeresult($result);
 
-    if(!empty($row) && sha1($row['salt'].sha1($row['salt'].sha1($password))) === $row['password'])
+    // sha1($row['salt'].sha1($row['salt'].sha1($password)))
+    if(!empty($row) && getDoubleSaltedHash($passwor, $row['salt']) === $row['password'])
     {
         $sql = 'UPDATE ' . USERS_TABLE . "
-        SET user_password = '".phpbb_hash($row['password'])."'
+        SET user_password = '".phpbb_hash($password)."'
         WHERE user_id = " . (int) $row['user_id'] . ";";
         $db->sql_query($sql);
 
@@ -45,4 +44,70 @@ function login_wbb_db($username, $password, $ip = '', $browser = '', $forwarded_
     }
 
     return login_db($username, $password, $ip, $browser, $forwarded_for);
+}
+
+
+
+/**
+ * Returns a salted hash of the given value.
+ *
+ * @param 	string 		$value
+ * @param	string		$salt
+ * @return 	string 		$hash
+ */
+function getSaltedHash($value, $salt) {
+    if (!defined('ENCRYPTION_ENABLE_SALTING') || ENCRYPTION_ENABLE_SALTING) {
+        $hash = '';
+        // salt
+        if (!defined('ENCRYPTION_SALT_POSITION') || ENCRYPTION_SALT_POSITION == 'before') {
+            $hash .= $salt;
+        }
+
+        // value
+        if (!defined('ENCRYPTION_ENCRYPT_BEFORE_SALTING') || ENCRYPTION_ENCRYPT_BEFORE_SALTING) {
+            $hash .= encrypt($value);
+        }
+        else {
+            $hash .= $value;
+        }
+
+        // salt
+        if (defined('ENCRYPTION_SALT_POSITION') && ENCRYPTION_SALT_POSITION == 'after') {
+            $hash .= $salt;
+        }
+
+        return encrypt($hash);
+    }
+    else {
+        return encrypt($value);
+    }
+}
+
+/**
+ * Returns a double salted hash of the given value.
+ *
+ * @param 	string 		$value
+ * @param	string		$salt
+ * @return 	string 		$hash
+ */
+function getDoubleSaltedHash($value, $salt) {
+    return encrypt($salt . getSaltedHash($value, $salt));
+}
+
+/**
+ * encrypts the given value.
+ *
+ * @param 	string 		$value
+ * @return 	string 		$hash
+ */
+function encrypt($value) {
+    if (defined('ENCRYPTION_METHOD')) {
+        switch (ENCRYPTION_METHOD) {
+            case 'sha1': return sha1($value);
+            case 'md5': return md5($value);
+            case 'crc32': return crc32($value);
+            case 'crypt': return crypt($value);
+        }
+    }
+    return sha1($value);
 }
